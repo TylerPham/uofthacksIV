cf4ec53351c96cca56691f34263aaca340797588
Cynthia (Qingwei) Li <cynngah@gmail.com>
2017-01-21 17:32:27 -0500

    add form

--- jobradar/apps/base/views.py
+++ jobradar/apps/base/views.py
@@ -1,8 +1,15 @@
 """Views for the base app"""
 
 from django.shortcuts import render
-
+from django.http import HttpResponse
 
 def home(request):
     """ Default view for the root """
     return render(request, 'base/home.html')
+
+def query_location(request):
+    return render(request, 'base/form.html')
+
+# def post_new(request):
+#     form = PostForm()
+#     return render(request, 'query_location.html', {'form': form})
