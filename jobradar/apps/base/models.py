8b55115793aaab158c2ffaaf3606a15fd64dfffe
Albert Untung <aulbert@aulb.local>
2017-01-21 16:16:13 -0500

    Models created

--- jobradar/apps/base/models.py
+++ jobradar/apps/base/models.py
@@ -2,13 +2,33 @@
 from django.db import models
 
 class Job(models.Model):
-    pass
+    id = models.AutoField(primary_key=True)
+    name = models.CharField('job name', max_length=50)
+    company_id = ForeignKeyField(Company)
+    url = models.CharField()
+    description = models.CharField()
+    still_active = models.CharField()
 
 class Company(models.Model):
-    pass
+	id = models.AutoField(primary_key=True)
+	name = models.CharField()
+	location_id = ForeignKeyField(Location)
+
+class Location(models.Model):
+	id = models.AutoField(primary_key=True)
+	latitude = DecimalField()
+	longitude = DecimalField()
+	city = models.CharField()
+	country = models.CharField()
 
 class Rating(models.Model):
-    pass
+	id = models.AutoField(primary_key=True)
+	score = IntegerField()
+	company_id = ForeignKeyField(Company)
 
 class Distance(models.Model):
-    pass
+    distance_id = models.AutoField(primary_key=True)
+    center_id = ForeignKeyField(Location)
+    location_id = ForeignKeyField(Location)
+    location_to_center = DecimalField()
+
