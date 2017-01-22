052af58f99b810a780163b416642904e2828e372
Chris Arnold <chris.arnold@utoronto.ca>
2017-01-21 20:42:09 -0500

    add geocodes

--- generate-jobs/src/generate_jobs.py
+++ generate-jobs/src/generate_jobs.py
@@ -1,19 +1,15 @@
 import requests
 import json
 
+INDEED_REQUEST = 'http://api.indeed.com/ads/apisearch?publisher=3922021424492903&v=2&format=json&co=ca&q=software&limit=100'
+GEOCODE_REQUEST = 'https://maps.googleapis.com/maps/api/geocode/json?key=AIzaSyD1EiOv9PGqo-At2h6d8icLYkXRmktEQSs'
+CITIES = ['Toronto', 'Vancouver', 'Calgary', 'Edmonton', 'Ottawa', 'Winnipeg', 'Hamilton', 'Regina', 'Halifax']
+
+
 """
 download virtualenv, enable it
 pip install requests
 """
-
-url = 'http://api.indeed.com/ads/apisearch?publisher=3922021424492903&v=2&format=json&co=ca&q=software&l=Toronto%2C+ON&limit=100'
-resp = requests.get(url)
-
-# print(resp.json())
-
-# print(resp.status_code)
-# print(resp.json())
-
 
 # clean the json output pulled from indeed.com in the format of
 # 		{
@@ -53,6 +49,59 @@ def clean_json(raw_json):
 	print(json.dumps(output))
 	return json.dumps(output)
 
-clean_json(resp.json())
-# print(clean_json(resp.json()))
+def encode_url(company_name):
+	result = ''
+	for char in company_name:
+		if char == ' ':
+			result += '%20'
+		elif char == "'":
+			result += '%27'
+		else:
+			result += char
+	return result
+
+def in_city(address_components, city):
+	for component in address_components:
+		if component['long_name'] == city:
+			return True
+	return False
+
+
+if __name__ == "__main__":
+
+	for city in CITIES:
+
+		file = open("cities/" + city + ".csv", "w")
+		company_list = []
+
+		indeed_resp = requests.get(INDEED_REQUEST + '&l=' + city)
+		
+		for posting in indeed_resp.json()['results']:
+
+			print posting['company'] + ', ' + city
+			company_name = encode_url(posting['company'])
+			geocode_resp = requests.get(GEOCODE_REQUEST + '&address=' + company_name)
+			json = geocode_resp.json()['results']
+
+			if json:
+				location = json[0]['geometry']['location']
+
+				lat = location['lat']
+				lng = location['lng']
+
+				# check if googe maps returns something that's in toronto
+				if in_city(json[0]['address_components'], city):
+
+					# do not add duplicates
+					if posting['company'] not in company_list:
+						company_list.append(posting['company'])
+
+						try:
+							file.write('{}, {}, {}\n'.format(posting['company'], lat, lng))
+						except UnicodeEncodeError:
+							pass
+
+
+
+
 
